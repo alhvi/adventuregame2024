8371261fbcd756e827ff4488b607d379326a7ab0
Alhvi Balcarcel <abalcarcel@gmail.com>
2024-11-05 16:19:19 -0600

    fixed camera clamp

--- Assets/_AdventureGame/Scripts/PlayerMovement.cs
+++ Assets/_AdventureGame/Scripts/PlayerMovement.cs
@@ -8,17 +8,25 @@ public class PlayerMovement : MonoBehaviour
     private float cameraRotationSpeed = 200f;
     [SerializeField]
     private GameObject cameraLookout;
+    private Quaternion rotationQuat = Quaternion.identity;
+    private float characterRotationTime = 0;
+    private float characterRotationSpeed = 5f;
+    [SerializeField]
+    private GameObject model;
+    [SerializeField]
+    private bool thirdPersonActive = true;
 
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
     }
 
-    private void CameraMovement() {
+    private void CameraMovement()
+    {
         float mousex = Input.GetAxis("Mouse X");
         float mousey = Input.GetAxis("Mouse Y");
 
-        float rotationAngleYaw = mousex * cameraRotationSpeed * Time.deltaTime; ;
+        float rotationAngleYaw = mousex * cameraRotationSpeed * Time.deltaTime;
         Quaternion yaw = Quaternion.AngleAxis(rotationAngleYaw, Vector3.up);
 
         float rotationAnglePitch = mousey * cameraRotationSpeed * Time.deltaTime;
@@ -26,7 +34,15 @@ public class PlayerMovement : MonoBehaviour
 
         Quaternion appliedPitch = cameraLookout.transform.rotation * pitch;
 
-        cameraLookout.transform.rotation =  yaw * appliedPitch;
+        if (FormatAngle(appliedPitch.eulerAngles.x) > 45)
+        {
+            appliedPitch = Quaternion.Euler(45f, appliedPitch.eulerAngles.y, appliedPitch.eulerAngles.z);
+        } else if (FormatAngle(appliedPitch.eulerAngles.x) < -5)
+        {
+            appliedPitch = Quaternion.Euler(-5f, appliedPitch.eulerAngles.y, appliedPitch.eulerAngles.z);
+        }
+
+        cameraLookout.transform.rotation = yaw * appliedPitch;
     }
 
     private void Update()
@@ -34,14 +50,51 @@ public class PlayerMovement : MonoBehaviour
         CameraMovement();
         float movx = Input.GetAxis("Horizontal");
         float movz = Input.GetAxis("Vertical");
-        Vector3 movementVector = new(movx, 0, movz);
+        Vector3 movementVector = Vector3.zero;
+
+        if (thirdPersonActive)
+        {
+
+            movementVector = (movx * cameraLookout.transform.right) + (movz * cameraLookout.transform.forward);
+            movementVector = new Vector3(movementVector.x, 0, movementVector.z);
+            movementVector.Normalize();
+        } else
+        {
+            movementVector = new Vector3(movx, 0, movz);
+        }
+
+
+
+
 
         if (movementVector.magnitude > 0)
         {
 
             characterController.SimpleMove(movementVector * speed);
-            transform.forward = movementVector.normalized;
-            //transform.forward = cameraLookout.transform.forward;
+            rotationQuat = Quaternion.LookRotation(movementVector);
+            characterRotationTime = 0;
         }
+
+        if (thirdPersonActive)
+        {
+
+            if (characterRotationTime * characterRotationSpeed < 1)
+            {
+                characterRotationTime += Time.deltaTime;
+                model.transform.rotation = Quaternion.Lerp(model.transform.rotation, rotationQuat, characterRotationSpeed * characterRotationTime);
+            }
+        }
+    }
+
+
+    private float FormatAngle(float angle)
+    {
+        angle %= 360;
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+
+        return angle;
     }
 }
